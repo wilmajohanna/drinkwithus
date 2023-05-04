be76e894f7c108e48b626d88c26bf29b96370482
Wilma Gotthardsson <93529076+wilmajohanna@users.noreply.github.com>
2023-05-04 22:42:26 +0200

    små fix

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const login_home = document.getElementById("login_home");
+const css_switch = document.getElementById("create_account");
+const close_button = document.getElementById("close_button");
+const cover_background = document.getElementById("cover");
+const register_box = document.getElementById("register");
+const login_box = document.getElementById("login");
+
+/* function popup_box(event) {
+
+
+} */
