e0283e5bb57d0025e30a07797e4c8050820319f0
Besha Gjosha <97993034+beshagjosha@users.noreply.github.com>
2023-05-05 15:28:31 +0200

    js. struktur till recept sidan

--- js/recipe.js
+++ js/recipe.js
@@ -1,10 +1,53 @@
+"use strict";
 
-//for..in loop för checkboxarna/instructions
+// Vänta tills DOM laddas innan koden körs
+document.addEventListener('DOMContentLoaded', () => {
+    // Fetch drink data från drinksData.JSON file
+    fetch('drinksData.json')
+        .then(response => response.json())
+        .then(data => {
+            const recipe = data[1]; //Använder index (recipe) data (drinksData.js)
 
-for (const inst in random_drink_instructions) {
-    document.getElementById("instructions").innerHTML += `
-    <input type="checkbox"><a>${random_drink_instructions[inst]}</a>
-    <br>`
-}
+            // hämtar DOM elements
+            const drinkName = document.getElementById('drink_name');
+            const rating = document.getElementById('rating');
+            const drinkInfo = document.getElementById('drink_info');
+            const drinkImage = document.querySelector("#drink_bg_image");
+            const ingredientsList = document.getElementById('ingredients');
+            const instructionsList = document.getElementById('instructions');
 
-document.querySelector("img").src = random_drink.image;
+            // Sätter värde för DOM elements
+            drinkName.textContent = recipe.name;
+            rating.innerHTML = '&starf;&starf;&starf;&starf;';
+            drinkInfo.textContent = recipe.description;
+            drinkImage.src = recipe.image;
+
+            //console.log(drinkImage);
+            //console.log(recipe);
+
+            // Fyller i ingredienslistan
+            for (const ingredient in recipe.ingredients) {
+                if (recipe.ingredients.hasOwnProperty(ingredient)) {
+                    ingredientsList.innerHTML += `<li><input type="checkbox"> ${recipe.ingredients[ingredient]}</li>`;
+                }
+            }
+
+            // Fyller i instructions listan
+            for (const instruction in recipe.instructions) {
+                if (recipe.instructions.hasOwnProperty(instruction)) {
+                    instructionsList.innerHTML += `<li><input type="checkbox"> ${recipe.instructions[instruction]}</li>`;
+                    // addListItem(instructionsList, `${instruction}: ${recipe.instructions[instruction]}`);
+                }
+            }
+        })
+        .catch(error => console.error(error));
+
+    // Hjälpfunktion för att lägga till ett listobjekt till ett givet listelement
+    function addListItem(list, text) {
+        const item = document.createElement('li');
+        item.textContent = text;
+        list.appendChild(item);
+    }
+});
+
+
