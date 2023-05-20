ce5c16dccbddab6088426503e8302b9fb8d13a83
Besha Gjosha <97993034+beshagjosha@users.noreply.github.com>
2023-05-20 16:28:30 +0200

    tagit bort recipe.js som inte behövs

--- recipepage/recipe.js
+++
@@ -1,4 +0,0 @@
-/* // Function to relocate to recipe page with drink name as query parameter
-function relocateToRecipe(drinkName) {
-    window.location.replace(`../recipepage/index.php?drink=${drinkName}`);
-} */
