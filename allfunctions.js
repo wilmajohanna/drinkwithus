da0338da136d96684ca4fd95047a742e93b9360c
Besha Gjosha <97993034+beshagjosha@users.noreply.github.com>
2023-05-20 17:38:04 +0200

    ändrade i hopp om att det ska funka

--- allfunctions.js
+++ allfunctions.js
@@ -1,3 +1,5 @@
+"use strict";
+/*
 const favourite_button = document.getElementById("favourites");
 console.log(favourite_button);
 
@@ -16,6 +18,31 @@ function loading_alert(alert) {
     alert_box.innerHTML = `<div class="loading_text">${alert}</div>`;
 }
 
-function remove_loading_alert(alert) {
+function remove_loading_alert() {
     document.querySelector("#white_box").remove();
+}
+*/
+
+const favourite_button = document.getElementById("favourites");
+
+if (favourite_button) {
+    favourite_button.addEventListener("click", relocateToFavourites);
+}
+
+function relocateToFavourites() {
+    window.location.replace("../favouritepage");
+}
+
+function loading_alert(alertMessage) {
+    let alert_box = document.createElement("div");
+    alert_box.id = "white_box";
+    document.body.prepend(alert_box);
+    alert_box.innerHTML = `<div class="loading_text">${alertMessage}</div>`;
+}
+
+function remove_loading_alert() {
+    let alert_box = document.querySelector("#white_box");
+    if (alert_box) {
+        alert_box.remove();
+    }
 }
