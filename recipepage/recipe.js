b18d2fc5b327ce9ca9a7973758e0b82d201c15af
victoriatanner99 <victoriatanner9915@hotmail.com>
2023-05-23 20:42:27 +0200

    recipe

---
+++ recipepage/recipe.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+
+async function renderRecipePage() {
+
+    const sentDrink = new URLSearchParams(window.location.search).get("drink");
+    let recipeRequest = new Request(`./recipe.php?drink=${sentDrink}`);
+    let data = await fetchThemeRecipe(recipeRequest);
+    let name = data.name;
+    let image = data.image;
+    let ingredients = data.ingredients;
+    let instructions = data.instructions;
+    let description = data.description;
+
+    console.log(data);
+
+    document.getElementById("drink_name").textContent = name;
+    document.getElementById("desc").innerHTML = description;
+    document.getElementById("drink_bg_image").setAttribute("src", image);
+
+    const ingredientList = document.getElementById("ingredient");
+    console.log(ingredients); // Log the ingredients array
+    ingredients.forEach(ingredient => {
+        const listItem = document.createElement("li");
+        listItem.textContent = ingredient;
+        ingredientList.appendChild(listItem);
+    });
+
+    const instructionList = document.getElementById("instruction");
+    instructions.forEach(instruction => {
+        const listItem = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        listItem.appendChild(checkbox);
+        listItem.appendChild(document.createTextNode(instruction));
+        instructionList.appendChild(listItem);
+    });
+
+}
+
+
+renderRecipePage();
+
---
+++ recipepage/recipe.php
@@ -0,0 +1,45 @@
+<?php
+ini_set("display_errors", 1);
+
+function sendJSON($message, $response_code = 200) { 
+    header("content-type: application/json");
+    http_response_code($response_code);
+    echo json_encode($message);
+    exit();
+}
+
+$name = "";
+$image = "";
+$ingredients = array();
+$instructions = array();
+$description = "";
+
+$sentDrink = $_GET["drink"]; 
+$drinksData = file_get_contents("../recipepage/drinksData.json");
+$drinks = json_decode($drinksData, true);
+
+foreach ($drinks as $drink) {
+    if ($drink["name"] == $sentDrink) {
+        $name = $drink["name"];
+        $image = $drink["image"];
+        $ingredients = $drink["ingredients"] ?? array();
+        $instructions = $drink["instructions"] ?? array();
+        $description = $drink["description"];
+        break;
+    }
+}
+
+
+// Skapa en array med relevant data
+$response = array(
+    "name" => $name,
+    "image" => $image,
+    "ingredients" => array_values($ingredients),
+    "instructions" => array_values($instructions),
+    "description" => $description
+);
+
+// Skicka JSON response till JavaScript (recipe.js)
+sendJSON($response);
+
+?>
