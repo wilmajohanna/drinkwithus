485965d5b1779db1ccd33c9524d31ea78c18fd39
victoriatanner99 <victoriatanner9915@hotmail.com>
2023-05-23 20:42:14 +0200

    Theme page

--- themepage/theme.js
+++ themepage/theme.js
@@ -1,12 +1,40 @@
 "use strict";
 
-document.querySelectorAll(".theme_container").forEach((item) =>
-    item.addEventListener("click", fetch_drink_page)
-);
+async function renderThemePage() {
+    const searchQuery = window.location.search;
+    const getParam = new URLSearchParams(searchQuery);
+    const paramValue = getParam.get("theme");
 
-// Funktion för att flytta till temasida med temanamn som query parameter
-function relocateToTheme(themeName) {
-    const previousPage = window.location.href; // Spara den nuvarande sidans URL som den föregående sidan
-    window.history.pushState({ previousPage }, "", ""); // Lägg till den föregående sidan i webbläsarens historikstack
-    window.location.replace(`../themepage/theme.php?theme=${themeName}`); // Byt till den nya sidan
-};
+
+    // Läs in JSON-filen med temadrinks
+    const themeRequest = new Request(`./theme.php?theme=${paramValue}`)
+    let data = await fetchThemeRecipe(themeRequest);
+    console.log(data);
+    // Hitta rätt drink baserat på temat
+    const selectedTheme = data;
+
+    selectedTheme.forEach(drink => {
+        // Skapa elementen för temadrinken'
+        console.log(drink.image);
+        const themeContainer = document.createElement('div');
+        themeContainer.className = 'theme_container';
+        themeContainer.style.backgroundImage = `url('${drink.image}')`;
+
+        const themeText = document.createElement('p');
+        themeText.className = 'theme_name';
+        themeText.textContent = drink.name;
+
+        themeContainer.appendChild(themeText);
+        document.getElementById('theme_wrapper').appendChild(themeContainer);
+
+        themeContainer.addEventListener("click", e => {
+            const drinkName = e.target.querySelector("p").textContent;
+            console.log(drinkName);
+            window.location = `../recipepage/recipe.html?drink=${drinkName}`
+        })
+    });
+}
+renderThemePage();
+
+
+
--- themepage/theme.php
+++ themepage/theme.php
@@ -1,28 +1,26 @@
 
 <?php
-
 ini_set("dislpay_errors", 1);
 
 $sentTheme = $_GET["theme"];
-$drinksData = file_get_contents ("../recipepage/drinksData.json");
-$themes = json_decode ($drinksData, true);
+$drinksData = file_get_contents("../recipepage/drinksData.json");
+$themes = json_decode($drinksData, true);
 
+$themeData = array();
 
-    /* <div id="theme_wrapper">
-        <?php 
-            foreach ($themes as $drinks) {
-                if($drinks["theme"] == $sentTheme){
-                    
-                    $theme = $drinks ["theme"];
-                    $name = $drinks ["name"];
-                    $theme_image = $drinks ["image"];
+foreach ($themes as $drinks) {
+    if ($drinks["theme"] == $sentTheme) {
+        $theme = $drinks["theme"];
+        $name = $drinks["name"];
+        $theme_image = $drinks["image"];
+    
         
-                    echo  "<div class='theme_container'>";
-                        echo "<img class='theme_pic' src= $theme_image alt=''>";
-                        echo "<div class='theme_text'>";
-                            echo "<h1  class='theme_name'>$name</h1>";
-                        echo "</div>";
-                    echo "</div>";
-                }  
-  
-    */
+        $object1 = ["theme" => $theme, "name" => $name, "image" => $theme_image];
+        array_push($themeData, $object1);
+    }
+}
+
+echo json_encode($themeData);
+exit();
+
+?>
