4d14e76873b927a3799043610a69f5ce06c736d5
Wilma Gotthardsson <93529076+wilmajohanna@users.noreply.github.com>
2023-05-23 12:30:53 +0200

    favourite.js

--- favouritepage/favourites.js
+++ favouritepage/favourites.js
@@ -1,11 +1,11 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-  
+
   async function send_to_favorites(event) {
     // Hämtar värdet av "name" inuti den klickade containern
     event.target.style.color = "red";
-    
+
     // LocalStorage är ett objekt. Här hämtar vi namnet på användarens namn som vi har sparat i nyckeln "username".
     let current_username = localStorage.getItem("username");
     let drink_name = event.target.parentElement.querySelector("p").textContent;
@@ -18,10 +18,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       await fetch("../favouritepage/favourites.php", {
-        method: "POST",
+        method: "PATCH",
         headers: { "content-type": "application/json" },
-        body: JSON.stringify(data_to_send),
-      });
+        body: JSON.stringify(data_to_send)
+
+      })
+
+
+        .then(response => response.json())
+        .then(data => {
+
+
+          document.getElementById("container").innerHTML =
+            <p>User ID: $()</p>;
+
+
+        });
+
+
+
     } catch (error) {
       console.log("An error occurred:", error);
     }
@@ -33,3 +48,11 @@ document.addEventListener("DOMContentLoaded", () => {
     like_button.addEventListener("click", send_to_favorites)
   );
 });
+
+/* function retrieve_favourites(event) {
+  const container = document.getElementById("container");
+
+
+
+}
+ */
