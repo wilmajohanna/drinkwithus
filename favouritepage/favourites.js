16c432892dfb0ad492846ed38588494916521bbb
Besha Gjosha <97993034+beshagjosha@users.noreply.github.com>
2023-05-24 16:31:07 +0200

    toggle på hjärtat

--- favouritepage/favourites.js
+++ favouritepage/favourites.js
@@ -4,8 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   async function send_to_favorites(event) {
     // Hämtar värdet av "name" inuti den klickade containern
-    event.target.style.color = "red";
+    /*     event.target.style.color = "red";
+     */
+    const heartsSpan = event.currentTarget;
+    heartsSpan.classList.toggle('liked');
 
+    if (heartsSpan.classList.contains('liked')) {
+      heartsSpan.style.color = 'red'; // Set heart color to red
+    } else {
+      heartsSpan.style.color = ''; // Remove inline color style to revert to original color
+    }
     // LocalStorage är ett objekt. Här hämtar vi namnet på användarens namn som vi har sparat i nyckeln "username".
     let current_username = localStorage.getItem("username");
     let drink_name = event.target.parentElement.querySelector("p").textContent;
@@ -24,23 +32,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
       })
 
-/* 
-        .then(response => response.json())
-        .then(data => {
-
-
-          document.getElementById("container").innerHTML =
-            <p>User ID: $()</p>;
-
-
-        }); */
+      /* 
+              .then(response => response.json())
+              .then(data => {
+      
+      
+                document.getElementById("container").innerHTML =
+                  <p>User ID: $()</p>;
+      
+      
+              }); */
 
 
 
     } catch (error) {
       console.log("An error occurred:", error);
     }
-    
+
     load_loggedOnPage()
   }
 
