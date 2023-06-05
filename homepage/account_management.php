5fd5bbd947bea7ea8c977ce5154704c111a09887
victoriatanner99 <victoriatanner9915@hotmail.com>
2023-06-05 21:15:22 +0200

    delete

--- homepage/account_management.php
+++ homepage/account_management.php
@@ -1,6 +1,8 @@
 <?php
 ini_set("display_errors", 1);
 
+$request_method = $_SERVER["REQUEST_METHOD"];
+
 function sendJSON($message, $statuscode = 200)
 {
     header("Content-Type: application/json");
@@ -12,14 +14,16 @@ function sendJSON($message, $statuscode = 200)
 $all_users = json_decode(file_get_contents("../popupbox/users.json"), true);
 $incomingUser = json_decode(file_get_contents("php://input"), true);
 
-
-if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
+if ($request_method == "DELETE") {
     foreach ($all_users as $userIndex => $user) {
         $username = $user["username"];
         if ($username == $incomingUser["username"]) {
             array_splice($all_users, $userIndex, 1);
             file_put_contents("../popupbox/users.json", json_encode($all_users, JSON_PRETTY_PRINT));
+            sendJSON("User deleted");
         }
     }
 }
+
+sendJSON("User not found", 404);
 ?>
